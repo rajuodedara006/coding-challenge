ca58bf639a8b31f930d0c8ad8332fa896325294a
Raju Odedara <rajub.odedara@gmail.com>
2021-10-08 00:34:25 -0700

    Fixed issue and updated render_callback() with standards

--- php/Block.php
+++ php/Block.php
@@ -62,68 +62,47 @@ class Block {
 	 * @param WP_Block $block      The instance of this block.
 	 * @return string The markup of the block.
 	 */
-	public function render_callback( $attributes, $content, $block ) {
-		$post_types = get_post_types(  [ 'public' => true ] );
-		$class_name = $attributes['className'];
-		ob_start();
-
-		?>
-        <div class="<?php echo $class_name; ?>">
-			<h2>Post Counts</h2>
-			<ul>
-			<?php
-			foreach ( $post_types as $post_type_slug ) :
-                $post_type_object = get_post_type_object( $post_type_slug  );
-                $post_count = count(
-                    get_posts(
-						[
-							'post_type' => $post_type_slug,
-							'posts_per_page' => -1,
-						]
-					)
-                );
-
-				?>
-				<li><?php echo 'There are ' . $post_count . ' ' .
-					  $post_type_object->labels->name . '.'; ?></li>
-			<?php endforeach;	?>
-			</ul><p><?php echo 'The current post ID is ' . $_GET['post_id'] . '.'; ?></p>
-
-			<?php
-			$query = new WP_Query(  array(
-				'post_type' => ['post', 'page'],
-				'post_status' => 'any',
-				'date_query' => array(
-					array(
-						'hour'      => 9,
-						'compare'   => '>=',
-					),
-					array(
-						'hour' => 17,
-						'compare'=> '<=',
-					),
-				),
-                'tag'  => 'foo',
-                'category_name'  => 'baz',
-				  'post__not_in' => [ get_the_ID() ],
-				  'meta_value' => 'Accepted',
-			));
-
-			if ( $query->found_posts ) :
-				?>
-				 <h2>Any 5 posts with the tag of foo and the category of baz</h2>
-                <ul>
-                <?php
-
-                 foreach ( array_slice( $query->posts, 0, 5 ) as $post ) :
-                    ?><li><?php echo $post->post_title ?></li><?php
-				endforeach;
-			endif;
-		 	?>
-			</ul>
+	public function render_callback($attributes, $content, $block)
+	{
+		global $post;
+		$post_types = get_post_types(['public' => true]);
+		$attributes = wp_parse_args($attributes, ['className' => '']);
+		ob_start(); ?>
+		<div class="<?php echo esc_attr($attributes['className']); ?>">
+			<h2><?php echo __('Post Counts', 'site-counts'); ?></h2>
+			<?php foreach ($post_types as $post_type_slug) : ?>
+				<p>
+					<?php
+					$post_type_object = get_post_type_object($post_type_slug);
+					$post_count       = wp_count_posts($post_type_slug);
+					echo $post_count->publish > 0 ? sprintf(
+						/* translators: 1: Post Count 2: Post Type */
+						_n(
+							'There is %1$d %2$s',
+							'There are %1$d %2$s',
+							intval($post_count->publish),
+							'site-counts'
+						),
+						$post_count->publish,
+						$post_count->publish > 1 ? $post_type_object->labels->name : $post_type_object->labels->singular_name
+					) : sprintf(
+						/* translators: %s: Post Type */
+						__('There is no Posts for %s', 'site-counts'),
+						$post_type_object->labels->name
+					); ?>
+				</p>
+			<?php endforeach; ?>
+			<?php if (isset($post)) : ?>
+				<p>
+					<?php
+					echo sprintf(
+						/* translators: %d: Current Post ID */
+						__('The current post ID is %d', 'site-counts'),
+						intval($post->ID)
+					); ?>
+				</p>
+			<?php endif; ?>
 		</div>
-		<?php
-
-		return ob_get_clean();
+		<?php return ob_get_clean();
 	}
 }
